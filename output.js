cfeab10ff6eb4db6eeb778a2788c1b5eb61d1da8
action <action@github.com>
2025-05-22 15:53:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Apr 01 2025 14:46:43 GMT+0000 (Coordinated Universal Time)
+//Thu May 22 2025 15:53:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("爱海盐");
